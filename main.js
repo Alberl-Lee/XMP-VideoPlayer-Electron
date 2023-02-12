8ba0720cd38822d7f22216911a15386dcb4c0ce2
Alberl <316293804@qq.com>
2023-02-12 22:50:19 +0800

    鍔犱笂tab缃戠珯

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const path = require("path");
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 968 * 1.25,
+    height: 600 * 1.25,
     frame: false,
 
     webPreferences: {
--- renderer.js
+++ renderer.js
@@ -1,3 +1,5 @@
+$(".tab-contents").addClass("fullheight");
+$(".tab-contents").children().addClass("fullheight");
 $(".tabs-no-border>ul>li").click(function (e) {
   $(this).addClass("is-active").siblings().removeClass("is-active");
   $(".tab-contents").children().eq($(this).index()).addClass("is-block").siblings().removeClass("is-block");
