f0333478a57c22374fb2f0fc4dd6fab295795ab8
Alberl <316293804@qq.com>
2023-02-13 17:20:02 +0800

    鎼炲ソ澶у潡甯冨眬

--- renderer.js
+++ renderer.js
@@ -1,5 +1,7 @@
-$(".tab-contents").addClass("fullheight");
-$(".tab-contents").children().addClass("fullheight");
+// $(".tab-contents").addClass("fullheight");
+// $(".tab-contents").children().addClass("fullheight");
+// $(".tab-contents").children().addClass("is-align-self-stretch1");
+
 $(".tabs-no-border>ul>li").click(function (e) {
   $(this).addClass("is-active").siblings().removeClass("is-active");
   $(".tab-contents").children().eq($(this).index()).addClass("is-block").siblings().removeClass("is-block");
